b81b63e01559a7aecd1c913d81eb31c98eec8849
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-03-27 22:18:29 +0530

    Solved 4 more advance pattern problems

---
+++ PracticePaper/level6/AdvPatternProb2.java
@@ -0,0 +1,45 @@
+/*
+ 
+PRINT A HOLLOW RHOMBUS
+
+    *****
+   *   *
+  *   *
+ *   *
+*****
+
+ */
+
+package PracticePaper.level6;
+
+public class AdvPatternProb2 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i=1; i<=n; i++) {
+
+            // spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            if ( i==1 || i==n ){
+                // completely filled stars
+                for (int j=1; j<=n; j++){
+                    System.out.print("*");
+                } 
+            } else {
+                // first and last star
+                System.out.print("*");
+                // Gaps between the stars
+                for (int j=1; j<=n-2; j++){
+                    System.out.print(" ");
+                }
+                // last star
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ PracticePaper/level6/AdvPatternProb3.java
@@ -0,0 +1,38 @@
+/*
+ 
+PRINT PASCAL'S TRIANGLE
+
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+
+ */
+
+package PracticePaper.level6;
+
+public class AdvPatternProb3 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i=0; i<n; i++) {
+
+            // initial spaces
+            for (int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+
+            
+            int num = 1;    
+
+            // pascal theory implementation
+            for (int j=0; j<=i; j++){
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+    }
+}
